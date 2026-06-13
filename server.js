5d067925f095068c712300cc94062957705ef1e1
Abraj101 <121937588+Abraj101@users.noreply.github.com>
2026-06-13 17:42:59 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -3,15 +3,20 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware to automatically parse incoming form payloads from Bitrix24
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // Serve static assets out of the root directory
 app.use(express.static(path.join(__dirname)));
 
-// Routes to deliver our interfaces
-app.get('/', (req, res) => {
+// Allow BOTH GET (browser) and POST (Bitrix24) for the installer dashboard
+app.all('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/handler.html', (req, res) => {
+// Allow BOTH GET and POST for your custom payment layout handler block
+app.all('/handler.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'handler.html'));
 });
 
